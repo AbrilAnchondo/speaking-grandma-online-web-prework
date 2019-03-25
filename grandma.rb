72eaec699a2c492cd74a671975e527dd93bf41dd
Abril Anchondo Reynaga <abril79reynaga@gmail.com>
2019-03-25 22:50:14 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,11 +2,10 @@
 def speak_to_grandma(message)
   if message != message.upcase
     return "HUH?! SPEAK UP, SONNY!"
-  elsif message == message.upcase
-    return "NO, NOT SINCE 1938!"
-  end
-  if message == "I LOVE YOU GRANDMA!"
+  elsif message == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
+  else 
+    return "NO, NOT SINCE 1938!"
   end
 end
     
